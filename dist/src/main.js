e2fc657cac5c6aedb651cc6a45a4ee9f57d16a8c
martinmuller <martin.mueller.de@outlook.com>
2021-12-01 23:38:58 +0100

    fixed mistake

--- dist/src/main.js
+++ dist/src/main.js
@@ -6,7 +6,11 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
-    app.enableCors();
+    app.enableCors({
+        origin: '*',
+        allowedHeaders: 'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    });
     await app.listen(4000);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
